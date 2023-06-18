22171080d7be8da7b5a1c9678925a45172b9e047
joindomain <joindomain@smfpersero.co.id>
2023-06-18 22:05:06 +0700

    New Added Create Real Product Testing

---
+++ Test Suites/Apps_MobileTest/Apps_FlashSale_appPendaki.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Apps_FlashSale_appPendaki</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>67eae72e-8c86-44af-99a2-f0d77e3a124f</testSuiteGuid>
+   <testCaseLink>
+      <guid>b08c2437-3450-4292-bb4d-9bd977418361</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_HappyDayShopping/FlashSale_AddProduct</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>56e5434e-5782-4503-ad1f-2a96c251e159</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_AppPendaki/OpenDetail</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
